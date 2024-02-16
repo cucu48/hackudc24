b3a0a3df3fd85fdfaeb3d546af6440ec33b47cb3
EncryptEx <limusina10@gmail.com>
2024-02-16 22:15:55 +0100

    call to api

--- dyn-form/app/page.tsx
+++ dyn-form/app/page.tsx
@@ -1,6 +1,26 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
 
-export default function Home() {
-  return (<h1>Hello World!</h1>);
+import {useState, useEffect } from 'react';
+
+
+function Page() {
+  const url = "http://131b6ea8-87b5-4141-969d-29d7f4ad6b58.mock.pstmn.io";
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(url+'/api/v1/formTypes/1').then((res) => res.json()).then((data) => {
+      setData(data);
+      console.log(data);
+    });
+    console.log(data);
+  }, []);
+
+
+  return (
+    <div>
+      <h1>Dynamic Form</h1>
+      
+    </div>
+  );
 }
+
+export default Page;
